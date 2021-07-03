0188bb010bef8fc170a7874d633eb67d6998f875
Julio Tain Sueiras <juliosueiras@gmail.com>
2021-07-03 15:56:55 -0400

    Updated for steins gate

--- games/windows/steinsGate/wrapper.nix
+++ games/windows/steinsGate/wrapper.nix
@@ -1,6 +1,10 @@
 { protonWrapperScript, wineWowPackages, winetricks, curl, krb5, keyutils, game, proton, lib, steamcmd, steam, writeText, steam-run, writeScript, writeScriptBin, gameFiles, lndir, realGameLocation, steamUserInfo, ... }:
 
-writeScriptBin game.name ''
+let
+  winetricksMod = winetricks.override {
+    wine = null;
+  };
+in writeScriptBin game.name ''
   ${
     protonWrapperScript {
       inherit game gameFiles proton lndir lib steamUserInfo steamcmd steam realGameLocation;
@@ -8,14 +12,18 @@ writeScriptBin game.name ''
   }
 
   ${steam-run}/bin/steam-run ${writeScript "fix-${game.name}" ''
+    export LD_LIBRARY_PATH=${lib.makeLibraryPath [ curl krb5 keyutils ]}:$LD_LIBRARY_PATH
     cd $HOME/games/${game.name}/
     export WINEDLLOVERRIDES="dxgi=n" 
     export DXVK_HUD=1
     export WINEPREFIX=$PROTON_PREFIX_HOME/pfx
     export STEAM_COMPAT_DATA_PATH=$PROTON_PREFIX_HOME
     export STEAM_COMPAT_CLIENT_INSTALL_PATH=$HOME/.steam/steam
-
-    $PROTON_HOME/proton waitforexitandrun ./_CommonRedist/DirectX/Jun2010/DXSETUP.exe
+    export PATH=$PATH:$PROTON_HOME/dist/bin
+    
+    $PROTON_HOME/proton waitforexitandrun dir 
+    ${winetricksMod} -q d3dx9 
+    #$PROTON_HOME/proton waitforexitandrun ./_CommonRedist/DirectX/Jun2010/DXSETUP.exe
     $PROTON_HOME/proton waitforexitandrun ./Launcher.exe
   ''}
 ''
